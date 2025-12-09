16415862216036c622ad42312c16c81c42ef72e2
Ricardo Ferreira <ricardoferreiran@gmail.com>
2025-12-09 22:52:49 +0100

    instalamos fontawesome

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,16 +2,19 @@ import { CommonModule } from '@angular/common';
 import { Component, inject, OnInit } from '@angular/core';
 import { TareasService } from './services/tareas.service';
 import { FormsModule } from '@angular/forms';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faTrash } from '@fortawesome/free-solid-svg-icons';
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, FormsModule],
+  imports: [CommonModule, FormsModule, FontAwesomeModule],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent implements OnInit{
+  faTrash = faTrash;
   private _tareasService = inject(TareasService); 
 
   public tareaInput : string = '';
